44fc6129b44391db087f664ce7c232436a4ed8c7
Kyuseung Han <han@etri.re.kr>
2020-05-19 14:46:13 +0900

    o. update

--- rvx_init.py
+++ rvx_init.py
@@ -1,10 +1,25 @@
+import platform
 import argparse
 import os
 import subprocess
 from pathlib import Path
 import re
 
+is_linux = (platform.system()=='Linux')
+is_windows = not is_linux
+
 git_list = frozenset(('rvx_init', 'rvx_util','rvx_dev_util','rvx_tools','rvx_ssw','rvx_synthesizer_binary','rvx_hwlib','rvx_binary'))
+
+def execute_shell_cmd(cmd:str, cwd:Path):
+	subprocess.run(args=[cmd], shell=True, cwd=cwd)
+
+def get_shell_output(cmd:str):
+	return subprocess.check_output(cmd, shell=True).decode()
+
+def make_executable(path:Path):
+	if path.is_file():
+		if is_linux:
+			execute_shell_cmd(f'chmod +x {path.name}', path.parent)
 
 class BitbucketInfo():
 	def __init__(self, cwd:Path):
@@ -59,7 +74,7 @@ class GitRepo():
 		self.bitbucket_info = bitbucket_info
 		self.path = path.resolve()
 		if self.path.is_dir():
-			contents = subprocess.check_output('git remote -v', shell=True, cwd=self.path).decode()
+			contents = get_shell_output('git remote -v')
 			self.git_name = re.compile(r'(\brvx_[a-zA-Z0-9_]+).git\b').findall(contents)[0]
 		else:
 			self.git_name = None
@@ -77,20 +92,20 @@ class GitRepo():
 		elif not self.path.is_dir():
 			self.git_name = git_name
 			git_addr = self.get_remote_addr()
-			subprocess.run(args=[f'git clone {git_addr}'], shell=True, cwd=self.path.parent)
+			execute_shell_cmd(f'git clone {git_addr}', cwd=self.path.parent)
 			if self.path.name!=self.git_name:
-				subprocess.run(args=[f'mv {self.path.name} {self.git_name}'], shell=True, cwd=self.path.parent)
+				execute_shell_cmd(f'mv {self.path.name} {self.git_name}', cwd=self.path.parent)
 
 	def update(self):
 		if self.path.is_dir():
-			subprocess.run(args=['git pull origin master'], shell=True, cwd=self.path)
+			execute_shell_cmd('git pull origin master', cwd=self.path)
 			
 	def set_repo(self):
 		if self.path.is_dir():
 			git_addr = self.get_remote_addr()
-			subprocess.run(args=[f'git remote set-url origin {git_addr}'], shell=True, cwd=self.path)
-			subprocess.run(args=[f'git remote set-url --push origin {git_addr}'], shell=True, cwd=self.path)
-			subprocess.run(args=['git remote -v'], shell=True, cwd=self.path)
+			execute_shell_cmd(f'git remote set-url origin {git_addr}', cwd=self.path)
+			execute_shell_cmd(f'git remote set-url --push origin {git_addr}', cwd=self.path)
+			execute_shell_cmd('git remote -v', cwd=self.path)
 	
 	def change_repo(self):
 		if self.path.is_dir():
@@ -143,12 +158,12 @@ if __name__ == '__main__':
 				break
 
 			elif target=='git_config':
-				subprocess.run(args=['git config --global credential.helper \'cache --timeout=864000\''], shell=True, cwd=cwd)
+				execute_shell_cmd('git config --global credential.helper \'cache --timeout=864000\'', cwd=cwd)
 
 			elif target=='git_kshan':
-				subprocess.run(args=['git config --global core.editor vim'], shell=True, cwd=cwd)
-				subprocess.run(args=['git config --global user.name \"Kyuseung Han\"'], shell=True, cwd=cwd)
-				subprocess.run(args=['git config --global user.email han@etir.re.kr'], shell=True, cwd=cwd)
+				execute_shell_cmd('git config --global core.editor vim', cwd=cwd)
+				execute_shell_cmd('git config --global user.name \"Kyuseung Han\"', cwd=cwd)
+				execute_shell_cmd('git config --global user.email han@etir.re.kr', cwd=cwd)
 
 			elif target=='bitbucket' or target=='bb':
 				bitbucket_info.generate()
@@ -203,8 +218,19 @@ if __name__ == '__main__':
 				else:
 					GitRepo(bitbucket_info,cwd).change_repo()
 
+			elif target=='update_script':
+				if is_linux:
+					cmd_list = []
+					cmd_list.append('cd ./rvx_init && git pull origin master')
+					cmd_list.append('make set_repo set_repo_sub')
+					cmd_list.append('make git_update')
+					update_script_file = cwd / 'update.sh'
+					update_script_file.write_text('\n'.join(cmd_list))
+					make_executable(update_script_file)
+
 			else:
 				print(f'wrong target: {target}')
 		#
 		if not is_gui_mode:
 			break
+
